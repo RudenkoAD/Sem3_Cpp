03e77e022847ae737128c99000350704e02af43e
RudenkoAD <rudenkoAD@phystech.edu>
2022-10-08 15:02:39 +0300

    lab2 progress

--- labs2/refactor.cpp
+++ labs2/refactor.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
 #include <set>
 #include <random>
+#include <fstream>
+#include <string>
 
 class State{
     protected:
         std::set<int> states;
     public:
+
         bool contains(int s) const {
             return states.count(s) > 0;
         }
+
         State(): states(){}
         State(std::set<int> src){
             states = src;
         }
-        State operator+ (const State& other) const{
-            std::set<int> newstates = states;
-            for(auto i = other.states.begin(); i!=other.states.end(); i++){
-                if(newstates.count(*i)>0){
-                    newstates.insert(*i);
-                }
-            }
-            return State(newstates);
-        }
-        State operator* (const State& other) const{
-            std::set<int> newstates = states;
-            for(auto i = newstates.begin(); i!=newstates.end();){
-                if(other.states.count(*i)>0){
-                    i = newstates.erase(i);
-                } else{
-                    i++;
-                }
-            }
-            return State(newstates);
-        }
+        friend State operator+ (const State& lha, const State& rha);
+        friend State operator- (const State& lha, const State& rha);
+        friend State operator* (const State& lha, const State& rha);
+        
 };
+
+State operator+ (const State& lha, const State& rha){
+    std::set<int> newstates = lha.states;
+    for(auto i = rha.states.begin(); i!=rha.states.end(); i++){
+        if(newstates.count(*i)>0){
+            newstates.insert(*i);
+        }
+    }
+    return State(newstates);
+}
+
+State operator- (const State& lha, const State& rha){
+    std::set<int> newstates = lha.states;
+    for(auto i = rha.states.begin(); i!=rha.states.end(); i++){
+        if(newstates.count(*i)>0){
+            newstates.erase(*i);
+        }
+    }
+    return State(newstates);
+}
+
+State operator* (const State& lha, const State& rha){
+    std::set<int> newstates = lha.states;
+    for(auto i = newstates.begin(); i!=newstates.end();){
+        if(rha.states.count(*i)>0){
+            i = newstates.erase(i);
+        } else{
+            i++;
+        }
+    }
+    return State(newstates);
+}
+
 
 class DiscreteState: public State {
 public:
@@ -60,13 +81,6 @@ class SetState: public State {
     }
 };
 
-class MultSegmentStates: public State {
-    MultSegmentStates(): State(){};
-    MultSegmentStates(std::set<std::tuple<int, int>> src): State(){
-
-    }
-};
-
 class ProbabilityTest {
 private:
     unsigned seed;
@@ -76,6 +90,11 @@ private:
 public:
     ProbabilityTest(unsigned seed, int test_min, int test_max, unsigned test_count): seed(seed), test_min(test_min),test_max(test_max), test_count(test_count) { }
     
+    int change_count(int a){
+        test_count = a;
+        return a;
+    }
+
     float operator()(State s) const {
         std::default_random_engine rng(seed);
         std::uniform_int_distribution<int> dstr(test_min,test_max);
@@ -87,13 +106,27 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
-    DiscreteState d(1);
-    SegmentState s(0,10);
-    //SetState ss({1, 3, 5, 7, 23, 48, 57, 60, 90, 99});
-    ProbabilityTest pt(10,0,100,100000);
-    std::cout << pt(d) << std::endl;
-    std::cout << pt(s) << std::endl;
-    //std::cout << pt(ss) << std::endl;
+int Operator(ProbabilityTest& pt, State& st, std::ostream& out, int const n){
+    for(int i=0; i<n; i+=100){
+        pt.change_count(i);
+        out<<pt(st)<<' ';
+    }
+    out<<std::endl;
     return 0;
+}
+
+int N = 100000;
+
+int main(int argc, const char * argv[]) {
+    //int seed;
+    //std::cin>>seed;
+    std::ofstream out("out.txt");
+    out<<"TEST"<<std::endl;
+    out.close();
+    //SegmentState ss = SegmentState(1, 10);
+    //DiscreteState ds = DiscreteState(11);
+    //ProbabilityTest pt(seed,0,100,N);
+    //State sum = ss + ds;
+    //Operator(pt, sum, out, N);
+    //out.close(); 
 }
